38b7d80ba33ca1766ec953642584b802bbc7d0c0
shohammallick25 <shohammallick528@gmail.com>
2025-07-04 23:53:04 +0600

    add

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'bookwave-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/style.css',
+  '/assets/js/script.js',
+  '/assets/images/hero-banner.png',
+  // Add more assets as needed
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
